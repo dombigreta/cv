e289c242cb938ec196de5bf4bbd149797e925cdd
dombigreta <dombigreta96@gmail.com>
2018-09-23 09:17:02 +0200

    adding authenticateUser  method

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -36,7 +36,28 @@ UserSchema.pre('save', function(next){
         user.password = hash;
         next();
     })
-})
+});
+
+UserSchema.statics.authenticateUser = function(email, password, callback){
+    User.findOne({email:email}).exec(function(err, user){
+        if(err){
+            return callback(err);
+        }
+        if(!user){
+            var error = new Error("User not found");
+            err.status = 401;
+            return callback(error);
+        }
+        bcrypt.compare(password, user.password,function(err, result){
+            if(result == true){
+                return callback(user);
+            }
+            else{
+                return callback();
+            }
+        })
+    });
+}
 
 var User = mongoose.model('User',UserSchema);
 module.exports = User;

1deea686c7bffc89a8bc80350ec9323ef0380be1
dombigreta <dombigreta96@gmail.com>
2018-09-24 05:11:30 +0200

    mocking loging vol01

--- client/src/Pages/Login.Component.tsx
+++ client/src/Pages/Login.Component.tsx
@@ -7,16 +7,45 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
 
-export default class LoginComponent extends React.Component{
+
+interface LoginComponentProps{
+}
+interface LoginComponentState{
+    useremail:string;
+    password:string;
+}
+export default class LoginComponent extends React.Component<LoginComponentProps,LoginComponentState>{
     
-    login = () => {
-        axios.post('/user/login',{email:'dombigreta96@gmail.com',password:'valamiasd'}).then(res => console.log(res))
+    constructor(props){
+        super(props);
+
+        this.state = {
+            useremail:'',
+            password:''
+        }
     }
+
+    login = () => {
+        console.log(this.state);
+        axios.post('/user/login',{email:this.state.useremail,password:this.state.password}).then(res => console.log(res))
+    }
+
+    handlePasswordFieldChange = (e) => {
+        this.setState({
+            password:e.target.value
+        });
+    }
+
+    handleUseremailFieldChange = (e) => {
+        this.setState({
+            useremail:e.target.value
+        });
+    } 
     
     render(){
         return(
         <div className="col-4 mx-auto mt-5">
-                <Card className="d-flex flex-column p-4" onClick={() => this.login()}>
+                <Card className="d-flex flex-column p-4">
                     <Typography style={{ marginBottom: '0.5rem',fontSize: '1.9rem',color:'#f44336'}} >
                             Login
                     </Typography>
@@ -24,7 +53,8 @@ export default class LoginComponent extends React.Component{
                         required
                         id="standard-uncontrolled"
                         label="Email"
-                        defaultValue="foo"
+                        value={this.state.useremail}
+                        onChange={this.handleUseremailFieldChange}
                         className="nothing"
                         margin="normal"/>
                         
@@ -32,11 +62,12 @@ export default class LoginComponent extends React.Component{
                         required
                         id="standard-uncontrolled"
                         label="Password"
-                        defaultValue="dufhkjg"
+                        value={this.state.password}
+                        onChange={this.handlePasswordFieldChange}
                         className="nothing"
                         type="password"
                         margin="normal"/>
-                    <Button className="primary">Login</Button>
+                    <Button className="primary" onClick={() => this.login()}>Login</Button>
                 </Card>
             </div>
         );
